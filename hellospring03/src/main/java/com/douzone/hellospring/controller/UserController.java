69ef76ebae04bbd4cdf4727408b76b3eb966efc8
insung93 <jis5780@gmail.com>
2021-06-02 17:10:52 +0900

    @ redirect

--- hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
+++ hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -3,7 +3,6 @@ package com.douzone.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /*
  * 
