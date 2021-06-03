fd6130e50ec9dbfcb8daf63408c1907ed81cbf9a
insung93 <jis5780@gmail.com>
2021-06-03 09:43:00 +0900

    @request param

--- hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
+++ hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -3,6 +3,8 @@ package com.douzone.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /*
  * 
@@ -27,5 +29,28 @@ public class UserController {
 		System.out.println(vo);
 		return "redirect:/";
 	}
-	//http://localhost:8080/hellospring03/user/join에서 POST요청
+	//http://localhost:8080/hellospring03/user/join에서 가입하기 버튼 POST요청
+	
+	@ResponseBody
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {
+		/*
+		 * 만일 n이라는 이름의 파라미터가 없는 경우 
+		 * 400 Bad Request 에러가 발생한다.
+		 */
+		System.out.println(name);
+		if(name==null) {
+			name = "";
+		}
+		return "UserController.update()";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value="n",required=true, defaultValue="") String name,
+							@RequestParam(value="a",required=true, defaultValue="0") int age) {
+
+		System.out.println("---" + name + ":" + age);
+		return "UserController.update()";
+	}
 }
