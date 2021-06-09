1fd779708fb04ad4c0ec25ee4e150d18ae28c378
insung93 <jis5780@gmail.com>
2021-06-09 11:43:00 +0900

    AOP test #finish

---
+++ aoptest/src/main/java/com/douzone/aoptest/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.douzone.aoptest.service;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.stereotype.Service;
+
+import com.douzone.aoptest.vo.ProductVo;
+
+@Service
+public class ProductService {
+	public ProductVo find(String name) {
+		System.out.println("[ProductService] finding...");
+//		if(1-1 == 0) {
+//			throw new RuntimeException("Find Exception");
+//		}
+		ProductVo vo = new ProductVo(name);
+		return vo;
+	}
+}
